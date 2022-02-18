af4fad21ace9711bbc282569c857fa7cb59c9b44
Maddi Wade <Madelinenwade@gmail.com>
2022-02-18 12:58:16 -0500

    Add project files.

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(7, 9);
+            Add(6.0m, 8.0m);
+            Add(12, 11, true);
+            Console.WriteLine(Add(12, 11, true));
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool isPlural)
+        {
+            var sum = x+ y;
+            if (sum == 1)
+            {
+                isPlural = true;
+                return ($"{sum} dollar");
+            }
+            else  
+            {
+                return ($"{sum} dollars");
+            }
+        }
+    }
+
+
+}
